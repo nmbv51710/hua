e2c78672b8fc26e9bde512ef3607c39bfc6c3131
Kell Paul <45677941+nmbv51710@users.noreply.github.com>
2021-01-01 20:40:11 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 bash <(curl -L https://raw.githubusercontent.com/XTLS/Xray-install/main/install-release.sh)
-wget --no-check-certificate -O /usr/local/etc/v2ray/config.json \
-"https://raw.githubusercontent.com/nmbv51710/v2ray/master/config.json.tcp"
-wget --no-check-certificate -O /usr/local/etc/xray/config.json \
-"https://raw.githubusercontent.com/nmbv51710/v2ray/master/config.xray.ss"
+wget --no-check-certificate -O /etc/v2ray/config.json "https://raw.githubusercontent.com/nmbv51710/hua/main/config.json.tcp"
+wget --no-check-certificate -O /etc/xray/config.json "https://raw.githubusercontent.com/nmbv51710/hua/main/config.xray.ss"
 systemctl start v2ray
 systemctl enable v2ray
 systemctl status v2ray
