35f49b00522e9b5612e239af5343cd32e174f709
Kell Paul <45677941+nmbv51710@users.noreply.github.com>
2021-01-18 21:23:00 +0800

    Add files via upload

---
+++ key.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+apt-get update -y
+apt-get install curl -y
+echo '============================
+      SSH Key Installer
+	 V1.0 Alpha
+	Author:Kirito
+============================'
+cd ~
+mkdir .ssh
+cd .ssh
+curl https://github.com/$1.keys > authorized_keys
+chmod 700 authorized_keys
+cd ../
+chmod 600 .ssh
+cd /etc/ssh/
+wget --no-check-certificate -O /etc/ssh/sshd_config https://git.io/JtIbm
+systemctl restart sshd
+cd ~
+rm -rf key.sh
