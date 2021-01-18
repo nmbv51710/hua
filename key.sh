5380d7073d4ed01aa7b75aac2b7ed905bf9fdcba
Kell Paul <45677941+nmbv51710@users.noreply.github.com>
2021-01-18 21:43:17 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -3,18 +3,17 @@ apt-get update -y
 apt-get install curl -y
 echo '============================
       SSH Key Installer
-	 V1.0 Alpha
-	Author:Kirito
+         V1.0 Alpha
+        Author:Kirito
 ============================'
-cd ~
-mkdir .ssh
-cd .ssh
+cd /root
+mkdir /root/.ssh
+cd /root/.ssh
 curl https://github.com/$1.keys > authorized_keys
 chmod 700 authorized_keys
-cd ../
+cd  /root
 chmod 600 .ssh
 cd /etc/ssh/
 wget --no-check-certificate -O /etc/ssh/sshd_config https://git.io/JtIbm
 systemctl restart sshd
-cd ~
-rm -rf key.sh
+rm -rf /root/key.sh
